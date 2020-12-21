054dc335da22ed411c3279b5a6863f9f2ddbf558
Friday <ren754203791@126.com>
2020-12-21 21:29:33 +0800

    shops

---
+++ src/components/goods/Cate.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!-- 面包屑导航区 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <el-row>
+        <!-- 添加分类按钮 -->
+        <el-col>
+          <el-button type="primary">添加分类</el-button>
+        </el-col>
+      </el-row>
+      <!-- 表格 -->
+      <!-- 分页区域 -->
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+</style>
