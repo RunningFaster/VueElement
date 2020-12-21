051b1b5e68e6c95cc4fa6d19755050e464d57fa7
Friday <ren754203791@126.com>
2020-12-21 19:55:32 +0800

    commit

---
+++ src/components/power/Roles.vue
@@ -0,0 +1,193 @@
+<template>
+  <div>
+    <!-- 面包屑导航区 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片试图 -->
+    <el-card>
+      <!-- 添加角色按钮 -->
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加角色</el-button>
+        </el-col>
+      </el-row>
+
+      <!-- 角色列表 -->
+      <el-table :data="rolesList"
+                border
+                stripe>
+        <!-- 展开列 -->
+        <el-table-column type="expand">
+          <template slot-scope="scope">
+            <!-- 通过动态指定 class 对象值 -->
+            <el-row :class="['bdbottom', i1 === 0 ? 'bdtop' : '', 'vcenter']"
+                    v-for="(item1, i1) in scope.row.children"
+                    :key="item1.id">
+              <!-- 渲染一级权限 -->
+              <el-col :span="5">
+                <el-tag @close="removeRightById(scope.row, item1.id)">{{item1.authName}}</el-tag>
+                <i class="el-icon-caret-right"></i>
+              </el-col>
+              <!-- 渲染二级权限 -->
+              <el-col :span="19">
+                <!-- 通过 for 循环嵌套渲染 二级权限菜单 -->
+                <el-row :class="[i2 !== 0 ? 'bdtop' : '', 'vcenter']"
+                        v-for="(item2, i2) in item1.children"
+                        :key="item2.id">
+                  <el-col :span="6">
+                    <el-tag closable
+                            type="success"
+                            @close="removeRightById(scope.row, item2.id)">{{item2.authName}}</el-tag>
+                    <i class="el-icon-caret-right"></i>
+                  </el-col>
+                  <!-- 三级权限 -->
+                  <el-col :span="18">
+                    <el-tag closable
+                            type="warning"
+                            v-for="(item3, i3) in item2.children"
+                            :key="item3.id"
+                            @close="removeRightById(scope.row, item3.id)">
+                      {{item3.authName}}
+                      {{i3}}
+                    </el-tag>
+                  </el-col>
+                </el-row>
+              </el-col>
+            </el-row>
+          </template>
+        </el-table-column>
+        <!-- 索引列 -->
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="角色名称"
+                         prop="roleName"></el-table-column>
+        <el-table-column label="角色描述"
+                         prop="roleDesc"></el-table-column>
+        <el-table-column label="操作"
+                         width="500px">
+          <template slot-scope="scope">
+            {{ scope.row.roleName }}
+            <el-button type="primary"
+                       icon="el-icon-edit"
+                       size="mini">编辑</el-button>
+            <el-button type="danger"
+                       icon="el-icon-delete"
+                       size="mini">删除</el-button>
+            <el-button type="warning"
+                       icon="el-icon-setting"
+                       size="mini"
+                       @click="showSetRightDialog(scope.row)">分配权限</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+
+    <!-- 分配权限的对话框 -->
+    <el-dialog title="分配权限"
+               :visible.sync="setRightDialogVisible"
+               width="50%">
+      <!-- 树型控件 -->
+      <!-- 通过 props 绑定树形控件的属性 -->
+      <!-- 通过 node-key 设置当前行的唯一标识信息 -->
+      <!-- default-checked-keys 当前数据中默认的选择数据 -->
+      <el-tree :data="rightsList" :props="treeProps" show-checkbox node-key="id" default-expand-all :default-checked-keys="defKeys"></el-tree>
+      <span slot="footer"
+            class="dialog-footer">
+        <el-button @click="setRightDialogVisible = false">取 消</el-button>
+        <el-button type="primary"
+                   @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+
+  </div>
+</template>
+
+<script>
+export default {
+  created () {
+    this.getRolesList()
+  },
+  data () {
+    return {
+      // 所有角色列表数据
+      rolesList: [],
+      // 分配权限对话框
+      setRightDialogVisible: false,
+      // 获取所有的权限数据
+      rightsList: [],
+      // 树型控件的属性
+      treeProps: {
+        label: 'authName',
+        children: 'children'
+      },
+      // 默认选择的节点的key值
+      defKeys: []
+    }
+  },
+  methods: {
+    async getRolesList () {
+      const { data: res } = await this.$http.get('roles')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.message)
+      this.rolesList = res.data
+    },
+    // 根据id删除对应的权限
+    async removeRightById (role, rightId) {
+      // 弹框提示用户是否确认
+      const confirmResult = await this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
+      if (confirmResult !== 'confirm') return this.$message.info('取消了删除！')
+      const { data: res } = await this.$http.delete(`roles/${role.id}/rights/${rightId}`)
+      if (res.meta.status !== 200) return this.$message.error(res.meta.message)
+      // 全局变量 和 局部变量
+      // 防止 整个列表的刷新
+      role.children = res.data
+      this.$message.success('删除成功！')
+    },
+    // 展示分配权限的对话框
+    async showSetRightDialog (role) {
+      // 获取所有权限列表
+      const { data: res } = await this.$http.get('rights/tree')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.message)
+      // 把所有的权限数据保存
+      this.rightsList = res.data
+      // 获取当前角色的所有的三级权限
+      console.log(role)
+      this.getLeafKeys(role, this.defKeys)
+      this.setRightDialogVisible = true
+    },
+    // 通过递归的方式，获取角色下的三级权限的id，并保存到 default 对象中
+    getLeafKeys(node, arr) {
+      if (!node.children) {
+        return arr.push(node.id)
+      }
+      node.children.forEach(item => this.getLeafKeys(item, arr))
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.el-tag {
+  margin: 7px;
+}
+
+.bdtop {
+  border-top: 1px solid #eee;
+}
+
+.bdbottom {
+  border-bottom: 1px solid #eee;
+}
+
+// 定一个剧中效果的 类
+.vcenter {
+  display: flex;
+  align-items: center;
+}
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 // 导入 element 组件
-import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree } from 'element-ui'
 
 // 组册 element 组件
 Vue.use(Button)
@@ -25,6 +25,8 @@ Vue.use(Switch)
 Vue.use(Tooltip)
 Vue.use(Pagination)
 Vue.use(Dialog)
+Vue.use(Tag)
+Vue.use(Tree)
 // 注册 message 到vue this 对象中
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
